70c5e5a0e287d713cbaa11d60bb1c55b217ed99a
moneswar99 <moniswarmoni509@gmail.com>
2026-04-29 10:29:42 +0530

    added uc7

---
+++ tictacapp/uc7.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+
+public class uc7{
+
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
+
+    static char computerSymbol = 'O';
+
+    public static void main(String[] args) {
+        printBoard();
+        computerMove();
+        printBoard();
+    }
+
+    // Function to print board
+    static void printBoard() {
+        System.out.println("Board:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    // UC7: Random valid move
+    static void computerMove() {
+        Random random = new Random();
+        int row, col;
+
+        while (true) {
+            row = random.nextInt(3); // 0 to 2
+            col = random.nextInt(3); // 0 to 2
+
+            // Check if the slot is empty
+            if (board[row][col] == '-') {
+                board[row][col] = computerSymbol;
+                System.out.println("Computer placed 'O' at (" + row + ", " + col + ")");
+                break;
+            }
+        }
+    }
+}
